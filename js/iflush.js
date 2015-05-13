f85706da2f3b2ce002b33bb1658f59e118aabb49
Mirman-comarley2016 <corey@mirman.org>
2015-05-13 13:25:38 -0700

    Added some js

--- js/iflush.js
+++ js/iflush.js
@@ -2,3 +2,18 @@ angular.module("flooshie", [])
 .controller("toiletCtrl", ["$scope",function($scope){
     
 }]);
+
+
+if(localStorage["todos"]){
+    var todos = JSON.parse(localStorage.getItem("todos"));
+} else {
+    localStorage.setItem("todos",JSON.stringify({}));
+    var todos = {} ;
+};
+
+var flush = Function() {
+    if button=pressed down {
+        flush 
+        
+    }
+}
