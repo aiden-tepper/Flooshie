52fc1dca5dc8bcae4256504c3ab503bdfdd9e418
mirman-aitepper2016 <aiden@mirman.org>
2015-05-14 13:59:33 -0700

    Added scope variables

--- js/iflush.js
+++ js/iflush.js
@@ -1,6 +1,10 @@
 angular.module("flooshie", [])
 .controller("toiletCtrl", ["$scope", "$http", function($scope, $http){
     
+    $scope.deviceId = "00e04c035ad5";
+    $scope.accessToken = "4fdb248ad771e101c196f31e5be93dffaa247d6994ebe490e303c1e55f1970ed";
+    $scope.URL = "https://api-http.littlebitscloud.cc/devices/" + $scope.deviceId;
+    
     $("#poo").on("click", function(){
         
     };
